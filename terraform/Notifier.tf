fd560f67bcc4b7550d58221812e9143701ab5272
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-30 22:55:03 -0300

    finalteste

--- terraform/Notifier.tf
+++ terraform/Notifier.tf
@@ -1,12 +1,12 @@
 module "network" {
-    source               = "./modules/network"
+    source               = "./modules/Network"
     vpc_cidr_block       = "${var.vpc_cidr_block}"
     vpc_dns_hostnames    = "${var.vpc_dns_hostnames}"
 
 }
 
 module "compute" {
-     source              = "./modules/compute"
+     source              = "./modules/Compute"
      ami                 = "${var.ec2_ami}
      instance_type       = "${var.ec2_instance_type}
 }
